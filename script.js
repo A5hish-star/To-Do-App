218df835008b249f2c196e5bc93584887e0a9c4b
A5hish-star <143864779+A5hish-star@users.noreply.github.com>
2023-10-08 13:15:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const input = document.getElementById("input-text");
+const list = document.getElementById("list");
+
+function addtask(){
+    if(input.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = input.value;
+        list.appendChild(li);
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7"
+        li.appendChild(span);
+    }
+    input.value = "";
+    savedata();
+}
+
+list.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("tick");
+        savedata();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        savedata();
+    }
+},false);
+
+function savedata(){
+    localStorage.setItem("data", list.innerHTML);
+}
+function showtask(){
+    list.innerHTML = localStorage.getItem("data");
+}
+showtask();
